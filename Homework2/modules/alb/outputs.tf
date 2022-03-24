dfd67b9e80fc2a62ff7f9c7615ca9e254175b164
Tally <43335535+tallyl@users.noreply.github.com>
2022-03-24 11:39:02 +0200

    Movingto Sigal's account

--- Homework2/modules/alb/outputs.tf
+++ Homework2/modules/alb/outputs.tf
@@ -1,7 +1,7 @@
-output "nlb_access_ip" {
-  value       = aws_lb.public_load_balancer[0].dns_name
-}
+//output "nlb_access_ip" {
+//  value       = aws_lb.public_load_balancer[0].dns_name
+//}
 
-output "alb_sg" {
-  value = aws_security_group.alb_sg[0].id
-}
+//output "alb_sg" {
+ // value = aws_security_group.alb_sg[0].id
+//}
